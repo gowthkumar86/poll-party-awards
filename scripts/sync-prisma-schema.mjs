430e62b062b7c049787cddf14263c8225e4138f4
gowthkumar86 <gowthkumar86@gmail.com>
2026-04-26 01:56:40 +0530

    Deploing on vercel

--- scripts/sync-prisma-schema.mjs
+++ scripts/sync-prisma-schema.mjs
@@ -1,6 +1,7 @@
 import fs from "node:fs";
 import path from "node:path";
 import process from "node:process";
+import 'dotenv/config'
 
 const ROOT = process.cwd();
 const templatePath = path.join(ROOT, "prisma", "schema.template.prisma");
